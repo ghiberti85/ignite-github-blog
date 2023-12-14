ff3e1346486a8a5b0dbbd21344cbe8bba1be7bd2
ghiberti85 <ghiberti85@gmail.com>
2023-12-14 16:04:11 -0300

    Create useDebounce file

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react"
+
+
+export function useDebouce<T>( value: T, delay?: number): T {
+    const [debouncedValue, setDebouncedValue] = useState<T>(value)
+
+    useEffect(() => {
+        const timer = setTimeout(() => setDebouncedValue(value), delay || 500)
+
+        function cleanup() {
+            clearTimeout(timer)
+        }
+        return cleanup
+    }, [value, delay])
+
+    return debouncedValue
+}
