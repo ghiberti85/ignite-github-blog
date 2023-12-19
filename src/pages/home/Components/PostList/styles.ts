0359528bc5eabf450b69bbb2cad1db09056c2199
ghiberti85 <ghiberti85@gmail.com>
2023-12-18 23:50:31 -0300

    Post List Component

---
+++ src/pages/home/Components/PostList/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const PostListContainer = styled.ul`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    list-style: none;
+    gap: 2rem;
+`
