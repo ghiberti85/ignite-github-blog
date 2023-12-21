e291655a4d97ff3c4304ef3bdf148f8cf1a65de7
ghiberti85 <ghiberti85@gmail.com>
2023-12-20 23:54:43 -0300

    Finishing PostHeader component

--- src/pages/Post/PostHeader/index.tsx
+++ src/pages/Post/PostHeader/index.tsx
@@ -1,7 +1,8 @@
 import { ArrowLeft, CalendarBlank, ChatCircle, GithubLogo } from "phosphor-react";
 import { GitHubLink } from "../../../components/GitHubLink";
-import { PostHeaderContainer, PostHeaderLinks, PostInfo, PostInfo } from "./styles";
+import { PostHeaderContainer, PostHeaderLinks, PostInfo } from "./styles";
 import { Link } from "react-router-dom";
+import { InfoGroup } from "../../Home/Components/AuthorCard/styles";
 
 interface Header {
     title: string
@@ -16,7 +17,7 @@ interface PostHeaderProps {
 }
 
 export function PostHeader({ header }: PostHeaderProps) {
-    return(
+    return (
         <PostHeaderContainer>
             <PostHeaderLinks>
                 <Link to={'/'}>
@@ -25,23 +26,23 @@ export function PostHeader({ header }: PostHeaderProps) {
                 </Link>
                 <GitHubLink content="Ver no GitHub" href={header.repositoryUrl} />
             </PostHeaderLinks>
+
+            <h2>{header.title}</h2>
+
+            <PostInfo>
+                <InfoGroup>
+                    <GithubLogo size={24} />
+                    <span>{header.user}</span>
+                </InfoGroup>
+                <InfoGroup>
+                    <CalendarBlank size={24} />
+                    <span>{header.time}</span>
+                </InfoGroup>
+                <InfoGroup>
+                    <ChatCircle size={24} />
+                    <span>{header.comments} comments</span>
+                </InfoGroup>
+            </PostInfo>
         </PostHeaderContainer>
-
-        <h2>{header.title}</h2>
-
-        <PostInfo>
-            <InfoGroup>
-                <GithubLogo size={24} />
-                <span>{header.user}</span>
-            </InfoGroup>
-            <InfoGroup>
-                <CalendarBlank size={24} />
-                <span>{header.time}</span>
-            </InfoGroup>
-            <InfoGroup>
-                <ChatCircle size={24} />
-                <span>{header.comments} comments</span>
-            </InfoGroup>
-        </PostInfo>
     )
 }
